4ecc3133054957687eb48eb27eb350db8387b2a7
devrafael-santos <raafael.cs@gmail.com>
2025-07-12 22:57:46 -0300

    Created Post Proprietario endpoint with exceptions, services and tests

--- src/main/java/com/raffasdev/cadastroVeiculos/repository/ProprietarioRepository.java
+++ src/main/java/com/raffasdev/cadastroVeiculos/repository/ProprietarioRepository.java
@@ -4,8 +4,7 @@ import com.raffasdev.cadastroVeiculos.domain.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, String> {
+    boolean existsByCpf(String cpf);
 }
